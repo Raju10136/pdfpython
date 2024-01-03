d08fc473c60eb91b24c78da2cddba2ee90a2a1a2
Raju10136 <108601999+Raju10136@users.noreply.github.com>
2024-01-03 18:46:41 +0530

    update the new code

--- app/controllers/pdf_controller.py
+++ app/controllers/pdf_controller.py
@@ -4,7 +4,7 @@ from app.utils.response_utils import success_response, error_response
 from app.services.data_service import get_data,get_data_new,update_data
 from app.exceptions.custom_exceptions import CustomException
 from app.services.filestorage_service import store_file_base64_array,retrieve_pdf_content
-from app.services.pdftools_service import merge_pdfs
+from app.services.pdftools_service import merge_pdfs,gs_compress,overlay_pdf_with_start_page
 import base64
 
 pdf_blueprint = Blueprint("pdf", __name__)
@@ -19,28 +19,75 @@ def merge_pdf():
         merged_array = store_file_base64_array(merge_payload,input_dir)
         output_file = input_dir + "/output.pdf"
         merge_pdfs(merged_array,output_file)
-        encoded_output = base64.b64encode(output_file.encode("utf-8")).decode("utf-8")
-        print("merged array ", encoded_output)
-        #data = {"output":encoded_output}
+        pdf_content = retrieve_pdf_content(output_file)
+        encoded_output = base64.b64encode(pdf_content).decode("utf-8")       
         return success_response(encoded_output)
     except CustomException as e:
         print("error ", e)
         return error_response(str(e), e.status_code)
 
-@pdf_blueprint.route("/get_pdf", methods=["GET"])
+@pdf_blueprint.route("/compress_pdf", methods=["POST"])
+def compress_pdf():
+    try:
+        payload = request.json.get('compress_data')
+        #print("input payrload data = ",merge_payload)
+        input_dir = DATA + "/compress"
+        out_array = store_file_base64_array(payload,input_dir)
+        output_file = input_dir + "/output.pdf"
+        gs_compress(out_array[0],output_file)
+        pdf_content = retrieve_pdf_content(output_file)
+        encoded_output = base64.b64encode(pdf_content).decode("utf-8")       
+        return success_response(encoded_output)
+    except CustomException as e:
+        print("error ", e)
+        return error_response(str(e), e.status_code)
+    
+@pdf_blueprint.route("/overlay_pdf", methods=["POST"])
+def overlay_pdf():
+    try:
+        payload = request.json.get('overlay_data')
+        #print("input payrload data = ",merge_payload)
+        input_dir = DATA + "/overlay"
+        out_array = store_file_base64_array(payload,input_dir)
+        merge_array = out_array[1:]
+        overlay_file = input_dir + "/overlay.pdf"
+        #merge pdf
+        merge_pdfs(merge_array,overlay_file)
+        # output file
+        output_file = input_dir + "/output.pdf"
+        # start page number
+        start_page_number = int(request.json.get('start_page',10))
+        #execute the overlay logic now
+        overlay_pdf_with_start_page(out_array[0],overlay_file,output_file,start_page_number)
+        # retrive the contet of the pdf file
+        pdf_content = retrieve_pdf_content(output_file)
+        # encode the content
+        encoded_output = base64.b64encode(pdf_content).decode("utf-8")  
+        # success response     
+        return success_response(encoded_output)
+    except CustomException as e:
+        print("error ", e)
+        return error_response(str(e), e.status_code)
+
+
+@pdf_blueprint.route("/get_pdf", methods=["POST"])
 def get_pdf():
     # Get the Base64-encoded location from the GET parameter
-    encoded_location = request.args.get('location', '')
+    encoded_location = request.json.get('location', '')
     try:
         # Decode the Base64-encoded location
         decoded_location = base64.b64decode(encoded_location).decode('utf-8')
+        print("decoed _location " , decoded_location)
         # Retrieve the PDF content using the function
         pdf_content = retrieve_pdf_content(decoded_location)
         # Set the content type to 'application/pdf' for the response
-        response = Response(pdf_content, content_type='application/pdf')
+        encoded_output = base64.b64encode(pdf_content).decode("utf-8")
+        #response = Response(pdf_content, content_type='application/pdf')
         # Optionally, set a filename for the downloaded PDF
-        response.headers['Content-Disposition'] = 'attachment; filename=output.pdf'
-        return response
+        #response.headers['Content-Disposition'] = 'attachment; filename=output.pdf'
+        #return response
+        return success_response(encoded_output)
     except Exception as e:
         # Handle any errors
+        print("error",str(e))
         return error_response(str(e), e.status_code)
--- app/services/pdftools_service.py
+++ app/services/pdftools_service.py
@@ -1,5 +1,14 @@
 from pikepdf import Pdf, Page, Rectangle
 import os
+import subprocess
+
+def generate_watermark_pdf(watermark_text, output_pdf):  
+    c = canvas.Canvas(output_pdf)
+    c.setFontSize(40)   
+    c.setFillColorRGB(0.7, 0.7, 0.7)
+    c.rotate(45)
+    c.drawString(180, 50, watermark_text)
+    c.save()
 
 def merge_pdfs(input_files,output_file):
     pdf = Pdf.new()
@@ -29,3 +38,58 @@ def delete_file_os(file_path):
         print(f"File '{file_path}' deleted successfully.")
     except OSError as e:
         print(f"Error deleting file '{file_path}': {e}")
+
+def gs_compress(input_pdf,output_pdf):
+    ghostscript_command = [
+    'gs',
+    '-sDEVICE=pdfwrite',
+    '-dCompatibilityLevel=1.4',
+    '-dPDFSETTINGS=/screen',
+    '-dNOPAUSE',
+    '-dQUIET',
+    '-dBATCH',
+    f'-sOutputFile={output_pdf}',
+    input_pdf
+    ]
+    # Execute the Ghostscript command
+    try:
+        subprocess.run(ghostscript_command, check=True)
+        print("Ghostscript command executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing Ghostscript command: {e}")
+# a service function to over the pdf
+def overlay_pdf_with_start_page(input,overlay,output,start_page):
+    try:
+        input_pdf = Pdf.open(input)
+        input_pdf_length=len(input_pdf.pages)
+        print("input pdf length " , input_pdf_length)
+        overlay_pdf = Pdf.open(overlay)
+        overlay_pdf_length=len(overlay_pdf.pages)
+        page_difference = input_pdf_length - (start_page + overlay_pdf_length)
+        print("overlay_length " , overlay_pdf_length, "  spage " , start_page , " diff = ", page_difference)
+        # Iterate through the pages of the input PDF
+        for page_num, input_page in enumerate(input_pdf.pages, start=1):
+            # Skip pages before the start_page
+            if page_num < start_page:
+                continue
+            # If overlay has pages left, overlay the current page
+            if page_num - start_page < len(overlay_pdf.pages):
+                input_final_page=Page(input_pdf.pages[page_num])
+                overlay_page =  Page(overlay_pdf.pages[page_num - start_page])
+                input_final_page.add_overlay(overlay_page, Rectangle(20, 130, 580, 750))
+        input_pdf.save(output)
+    except  Exception as e:
+        print(f"Error executing overlay command: {e}")
+
+# adding water mark
+def add_watermark(input_pdf, output_pdf, watermark_pdf):
+    # Open the original PDF
+    with Pdf.open(input_pdf) as pdf:
+        wpdf = Pdf.open(watermark_pdf)
+        # Loop through each page
+        for page_num in range(len(pdf.pages)):
+            destination_page = Page(pdf.pages[page_num])
+            thumbnail = Page(wpdf.pages[0])
+            destination_page.add_underlay(thumbnail, Rectangle(0, 0, 590, 800))
+        # Save the watermarked PDF to a new file
+        pdf.save(output_pdf)
