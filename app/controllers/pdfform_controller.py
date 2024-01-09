9f597254968496e310cc48b3cfded2ed941900f5
Raju10136 <108601999+Raju10136@users.noreply.github.com>
2024-01-09 17:38:14 +0530

    ok

--- app/controllers/pdfform_controller.py
+++ app/controllers/pdfform_controller.py
@@ -4,7 +4,7 @@ from app.utils.response_utils import success_response, error_response
 from app.services.data_service import get_data,get_data_new,update_data
 from app.exceptions.custom_exceptions import CustomException
 from app.services.filestorage_service import store_file_base64_array,retrieve_pdf_content
-from app.services.pdfform_service import read_pdf_form_fields,fill_form_fields
+from app.services.pdfform_service import read_pdf_form_fields,fill_form_fields,generate_template_pdf
 from app.services.encryption_service import base_64_decode
 
 
@@ -14,20 +14,30 @@ DATA="E:/test/data/pdfdata"
 @pdfform_blueprint.route("/get_fields", methods=["POST"])
 def get_fields():
     try:
-        payload = request.json.get('pdf_data')
-        #print("input payrload data = ",merge_payload)
+        payload = request.json.get('pdf_data')      
         input_dir = DATA + "/pdfform"
-        out_array = store_file_base64_array(payload,input_dir)
-        #output_file = input_dir + "/output.pdf"
-        #pdf_location = input_dir + "/temptestfill.pdf" 
+        out_array = store_file_base64_array(payload,input_dir)      
         print("enterering in read")
-        fields = read_pdf_form_fields(out_array[0])
-        #merged_array = store_file_base64_array(merge_payload,input_dir)
-        #output_file = input_dir + "/output.pdf"
-        ##merge_pdfs(merged_array,output_file)
-        #@pdf_content = retrieve_pdf_content(output_file)
-        #encoded_output = base64.b64encode(pdf_content).decode("utf-8")
-        data = {"test":"test"}       
+        fields = read_pdf_form_fields(out_array[0])           
+        return success_response(fields)
+    except CustomException as e:
+        print("error ", e)
+        return error_response(str(e), e.status_code)
+    
+@pdfform_blueprint.route("/generate_template", methods=["POST"])
+def generate_template():
+    try:
+        print("testing input template")
+        payload = request.json.get('template')      
+        input_dir = DATA + "/pdftempalte"
+        print("pdf storage ")
+        out_array = store_file_base64_array([payload],input_dir)      
+        print("enterering in read")
+        fields = request.json.get('fields') 
+        print("got the fields")
+        output_loc = input_dir + "/output.pdf"
+        print("try creating generate pdf")
+        generate_template_pdf(out_array[0],fields,output_loc)          
         return success_response(fields)
     except CustomException as e:
         print("error ", e)
--- app/services/pdfform_service.py
+++ app/services/pdfform_service.py
@@ -4,6 +4,7 @@ import subprocess
 import pdfkit
 # this is to generate water mark pdf with single page
 from reportlab.pdfgen import canvas
+import PyPDF2
 from PyPDF2 import PdfReader, PdfWriter
 
 
@@ -13,7 +14,6 @@ def read_pdf_form_fields_old(pdf_path):
            reader = PdfReader(pdf_path)
            fields = reader.get_form_text_fields()
            print("pdf fileds " , fields)
-
     except Exception as e:
         print(f"Error: {str(e)}") # PyMuPDF
 
@@ -21,8 +21,7 @@ def read_pdf_form_fields(pdf_path):
     form_field_info = []
     try:
         with open(pdf_path, 'rb') as pdf_file:
-            pdf_reader = PdfReader(pdf_file)
-            
+            pdf_reader = PdfReader(pdf_file)            
             for page_num in range(len(pdf_reader.pages)):
                 #page_info = {'page_number': page_num + 1, 'fields': []}
                 page_fileds =[]
@@ -83,4 +82,42 @@ def fill_form_fields(src_loc,page_fields,dest_loc):
     # write "output" to PyPDF2-output.pdf
     with open(dest_loc, "wb") as output_stream:
         writer.write(output_stream)
+
+
+def generate_template_pdf(src_loc,page_fields,dest_loc):
+    try:
+         reader = PdfReader(src_loc)
+         writer = PdfWriter()
+         print("page writer name as target ")
+         for page_num in range(len(reader.pages)):
+             page = reader.pages[page_num]
+             print("enteerd in page number " , page_num)       
+             if 0 <= page_num < len(page_fields):
+                fields = page_fields[page_num]
+                for obj in fields:    
+                    value = obj['value']
+                    name = obj['name']
+                    print("name of the filed and value of the field " , name ,  " V = " , value)
+                    if(value=="newsoft"):
+                        print("entered  in pdf page adding a document")
+                        llx = 100
+                        lly = 150
+                        urx = 200
+                        ury = 500
+                         # Create a new text field
+                        text_field = PyPDF2.pdf.GenericTextObject.createBlankFrom(page, fieldType='/Tx') 
+                        text_field.update({
+                             PyPDF2.generic.createStringObject('/T'):
+                             PyPDF2.generic.createStringObject("test"),
+                             PyPDF2.generic.createStringObject('/V'): PyPDF2.generic.createStringObject(value),
+                             PyPDF2.generic.createStringObject('/Rect'): PyPDF2.generic.createArrayObject(
+                            [llx, lly, urx, ury])})
+                        page[PyPDF2.generic.NameObject("/Annots")].append(text_field)
+                #value= obj['value']   
+             writer.add_page(page)      
+    # write "output" to PyPDF2-output.pdf
+         with open(dest_loc, "wb") as output_stream:
+            writer.write(output_stream)
+    except Exception as e:
+        print(f"Error: {str(e)}")
 
